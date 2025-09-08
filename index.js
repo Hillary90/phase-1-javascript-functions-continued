6cd73a59a5706bf39487dd11f6e33d331f205d9f
Hillary Tanui <hillarytanui80@gmail.com>
2025-09-08 15:13:12 +0300

    initial  commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(fun="roller-skate"){
+    return `This Saturday, I want to ${fun}!`
+}
+
+
+function mondayWork(action="go to the office"){
+    return `This Monday, I will ${action}.`
+
+}
+
+function wrapAdjective(wrapper = "*") {
+  return function(adjective = "a hard worker") {
+    return `You are ${wrapper}${adjective}${wrapper}!`;
+  }
+}
+console.log(wrapAdjective("||"))
